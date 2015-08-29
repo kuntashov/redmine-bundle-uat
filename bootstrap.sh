c67f0b81e7e2f9d67d94e85714f89f6f038f4c09
Alexey Lustin <alustin@silverbulleters.org>
2015-08-29 23:24:58 +0300

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,61 @@
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
+
+install 'development tools' build-essential
+
+echo installing russian locale
+install language-pack-RU
+
+# Needed for docs generation.
+locale-gen ru_RU
+locale-gen ru_RU.UTF-8
+
+update-locale LANG=ru_RU.UTF-8 LANGUAGE=ru_RU.UTF-8 LC_ALL=ru_RU.UTF-8
+
+install Ruby ruby2.2 ruby2.2-dev
+update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
+update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
+
+install Git git
+install SQLite sqlite3 libsqlite3-dev
+install memcached memcached
+# install Redis redis-server
+# install RabbitMQ rabbitmq-server
+
+install PostgreSQL postgresql postgresql-contrib libpq-dev
+#TODO - check if the role exist or database exist
+sudo -u postgres createuser --superuser redmine
+
+sudo -u postgres createdb -O redmine redmine-tests
+sudo -u postgres createdb -O redmine redmine-developer
+
+#debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+#debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+#install MySQL mysql-server libmysqlclient-dev
+#mysql -uroot -proot <<SQL
+#CREATE USER 'rails'@'localhost';
+#CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+#CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+#GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
+#GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
+#GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
+#SQL
+
+install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
+install 'ExecJS runtime' nodejs
+
+
+
+echo 'all set, rock on!'
