0a3e2558af475da4085ec6e7eadfe1333f8f3247
Alexey Lustin <alustin@silverbulleters.org>
2015-08-30 09:42:25 +0300

    add the first list to testing compatible

--- redmine-plugins.sh
+++ redmine-plugins.sh
@@ -1,1 +1,34 @@
 
+echo  a_common_libs 1.1.5
+echo  advanced_roadmap_v2 2.4.2
+echo  clipboard_image_paste 1.10
+echo  custom_menu 1.5.0
+echo  extra_queries 2.0.0
+echo  global_roles 2.0.0
+echo  planner 0.5
+echo  progressive_projects_list 2.0.1
+echo  quick_edit 0.0.9
+echo  quick_view 0.1.0
+echo  redmine_agile 1.3.9
+echo  redmine_backlogs v1.0.6
+echo  redmine_ckeditor 1.1.2
+echo  redmine_code_review 0.7.0
+echo  redmine_contacts 4.0.1
+echo  redmine_custom_workflows 0.1.4
+echo  redmine_dmsf 1.5.3
+echo  redmine_graphs 0.1.0
+echo  redmine_issue_tabs 1.1.0
+echo  redmine_lightbox2 0.2.2
+echo  redmine_monitoring_controlling 0.1.1
+echo  redmine_per_project_formatting 0.0.4
+echo  redmine_products 2.0.0
+echo  redmine_webhook 0.0.1
+echo  redmine_work_time 0.3.0
+echo  redmine_workflow_enhancements  0.4.0
+echo  rm_user_mentions 1.0.0
+echo  rmplus_devtools 1.0.0
+echo  stuff_to_do_plugin 0.4.2
+echo  time_logger 0.5.2.3
+echo  unread_issues 1.5.0
+echo  usability 1.3.0
+
