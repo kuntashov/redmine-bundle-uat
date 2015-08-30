73f660f55de6659671b49d0728d58849fa349da0
Alexey Lustin <alustin@silverbulleters.org>
2015-08-30 09:18:05 +0300

    add the structure, first readme and licence. work in progress...

--- redmine-core.sh
+++ redmine-core.sh
@@ -1,1 +1,21 @@
+echo enable redmine core (current master)
 
+cd /vagrant/
+git submodule update
+
+echo starting to bundle build
+
+cd ./redmine-core
+
+bundle install
+bundle update
+
+cp /vagrant/config/database.yml ./config/
+
+echo provisioning db and others
+
+bundle exec rake generate_secret_token
+
+bundle exec rake db:migrate
+
+RAILS_ENV=development bundle exec rake db:migrate
